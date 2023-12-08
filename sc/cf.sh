3c76eaa42854792e29ef0d9365de415f63cb19f9
basir96 <basirbinyati@gmail.com>
2023-12-08 22:00:51 +0700

    bebas ya

---
+++ sc/cf.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sleep 0.5
+echo "Memulai Install CloudF"
+sleep 1
+
+wget https://raw.githubusercontent.com/basir96/scvip/ipuk/CDN/A/I/U/E/O/cfo.sh
+chmod +x cfo.sh
+./cfo.sh
+rm -f cfo.sh
+
+echo -e "Powered By RIDWAN"
